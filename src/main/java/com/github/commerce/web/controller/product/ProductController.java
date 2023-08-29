2cbf7be997640ba32efee01b4b0fbf1525d60697
MoonJongHyeon1095 <109948801+MoonJongHyeon1095@users.noreply.github.com>
2023-08-29 12:51:33 +0900

    Docs: docker-compose 실행 확인 및 CICD용 헬스체크 API 추가

--- src/main/java/com/github/commerce/web/controller/product/ProductController.java
+++ src/main/java/com/github/commerce/web/controller/product/ProductController.java
@@ -1,4 +1,18 @@
 package com.github.commerce.web.controller.product;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@Slf4j
+@RestController
 public class ProductController {
+
+
+    @GetMapping("v1/api/navi")
+    public ResponseEntity<String> getHealthCheck(){
+        return ResponseEntity.ok("안녕! 물고기는 고마웠어요!");
+    }
 }
