7e31ca96b3bf6b79979f6a9f63c2b338fb310f5c
MoonJongHyeon1095 <109948801+MoonJongHyeon1095@users.noreply.github.com>
2023-08-30 10:36:48 +0900

    Feat: add cart entity, review entity

--- src/main/java/com/github/commerce/entity/Cart.java
+++ src/main/java/com/github/commerce/entity/Cart.java
@@ -1,4 +1,38 @@
 package com.github.commerce.entity;
 
+import lombok.*;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "carts")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class Cart {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "cart_id", nullable = false)
+    private Long cartId;
+
+//    @ManyToOne(fetch = FetchType.LAZY)
+//    @JoinColumn(name = "user_id")
+//    private User user;
+
+//    @ManyToOne(fetch = FetchType.LAZY)
+//    @JoinColumn(name = "product_id")
+//    private Product product;
+
+    @Column(name = "quantity")
+    private Integer quantity;
+
+    @Column(name = "order_state")
+    private Integer orderState;
+
+    @Column(name = "created_at")
+    private LocalDateTime createdAt;
 }
