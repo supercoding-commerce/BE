73433c382ad87f1333927b4eda6d47f5faaec644
Kyoungwon Min <mkkw2047@gmail.com>
2023-09-01 01:02:41 +0900

    chore: 안 쓰는 라이브러리 삭제

--- src/main/java/com/github/commerce/CommerceApplication.java
+++ src/main/java/com/github/commerce/CommerceApplication.java
@@ -3,7 +3,6 @@ package com.github.commerce;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
-import org.springframework.scheduling.annotation.EnableAsync;
 
 
 @SpringBootApplication(exclude = SecurityAutoConfiguration.class) //spring security를 임시로 꺼둘 때 사용합니다.
