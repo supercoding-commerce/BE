3a1e7c7b9828cf138bf84a2c3a727412221b4ed5
doit-zero <131241458+doit-zero@users.noreply.github.com>
2023-09-07 20:57:08 +0900

    refacor : 상품 수정시 swagger
    swagger보이는 부분 수정

--- src/main/java/com/github/commerce/web/controller/product/ProductController.java
+++ src/main/java/com/github/commerce/web/controller/product/ProductController.java
@@ -51,7 +51,7 @@ public class ProductController {
     @ApiOperation(value = "상품 식별값을 입력하여 단일의 product 레코드를 수정합니다.")
     @PatchMapping(value = "/{product_id}")
     public ResponseEntity<?> updateProduct(@PathVariable("product_id") Long productId,
-                                           @RequestPart ProductRequest productRequest,
+                                           @ModelAttribute ProductRequest productRequest,
                                            @AuthenticationPrincipal UserDetailsImpl userDetails
                                            ) {
         Long profileId = userDetails.getUser().getId();
