f71951feedceee02f8a03010705d9693648d46c1
dabin <113006964+dabinHam@users.noreply.github.com>
2023-09-07 12:06:59 +0900

    refactor : ERD변경 후 Reapository수정 및 추가
    refactor : ERD변경 후 Reapository수정 및 추가

---
+++ src/main/java/com/github/commerce/repository/payment/PayMoneyRepository.java
@@ -0,0 +1,16 @@
+package com.github.commerce.repository.payment;
+
+import com.github.commerce.entity.PayMoney;
+import com.github.commerce.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface PayMoneyRepository extends JpaRepository<PayMoney, Long> {
+
+    // 보유한 페이머니 , findTop1By -> 1개만 가져온다.
+    Optional<PayMoney> findTop1ByUsersOrderByIdDesc(User user);
+
+}
