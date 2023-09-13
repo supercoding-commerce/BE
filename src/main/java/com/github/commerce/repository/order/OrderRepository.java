8e1b820064e9bbcf201f6fdd1dd28203686fb02b
Kyoungwon Min <mkkw2047@gmail.com>
2023-09-14 01:52:44 +0900

    feat: 주기적으로 회원 등급 조정 기능 추가
    매월 1일에 회원별 구매 금액 합산해서 회원 등급 조정 기능 추가

--- src/main/java/com/github/commerce/repository/order/OrderRepository.java
+++ src/main/java/com/github/commerce/repository/order/OrderRepository.java
@@ -5,10 +5,14 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
 
+import java.time.LocalDateTime;
 import java.util.List;
+import java.util.Map;
 import java.util.Optional;
 
+@Repository
 public interface OrderRepository extends JpaRepository<Order, Long> {
     @Query(
             "SELECT o FROM Order o " +
@@ -63,4 +67,9 @@ public interface OrderRepository extends JpaRepository<Order, Long> {
                     "AND o.orderState in (2, 3, 4, 5) "
     )
     Optional<Order> validatePaidOrderByOrderId(Long orderId);
+
+    @Query("SELECT SUM(o.totalPrice) as totalPrice, o.users.id as userId FROM Order o " +
+            "WHERE o.createdAt >= :oneMonthBefore and o.orderState > 1 GROUP BY o.users.id "
+            )
+    List<Map<String, Object>> getUserTotalPriceFromOneMonth(LocalDateTime oneMonthBefore);
 }
