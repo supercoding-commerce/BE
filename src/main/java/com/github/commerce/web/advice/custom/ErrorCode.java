6efd02d663b6236b85185bc7c8ab90d922a28ffb
dabin <113006964+dabinHam@users.noreply.github.com>
2023-09-17 11:46:28 +0900

    feat : 임시 충전기능 추가
    - 현재 외부모듈과 연결이 지연됨에 따라 테스트에서 페이머니 필요시 불편함이 있어
    테스트 개발시에만 사용할 충전기능 추가완료.
    - 회원이면 누구나 백만 페이머니씩 충전이 됩니다.
     - 새로운 회원인 경우에만 기능적용이 가능합니다. 기존 페이를 DB상에서 넣은경우 에러 발생합니다.
     - 급하게 추가하느라 예외처리가 완벽히 되어있지 않습니다 .

--- src/main/java/com/github/commerce/web/advice/custom/ErrorCode.java
+++ src/main/java/com/github/commerce/web/advice/custom/ErrorCode.java
@@ -28,8 +28,22 @@ public enum ErrorCode {
 
     // 찜
     //404
-    NOT_FOUND_WISHLIST(HttpStatus.NOT_FOUND,"찜 등록된 상품이 존재하지 않습니다.");
+    NOT_FOUND_WISHLIST(HttpStatus.NOT_FOUND,"찜 등록된 상품이 존재하지 않습니다."),
 
+    // 충전
+    CHARGE_PAY_MONEY_SELLER_OPERATION_FORBIDDEN(HttpStatus.FORBIDDEN, "SELLER는 이 작업을 수행할 수 없습니다."),
+
+    // PayMoney
+    PAY_MONEY_NO_RECORDS_FOUND(HttpStatus.BAD_REQUEST,"해당 사용자의 페이머니 내역이 없습니다"),
+    PAYMENT_INSUFFICIENT_BALANCE(HttpStatus.BAD_REQUEST,"잔액이 부족합니다."),
+    PAYMENT_INVALID_COUPON( HttpStatus.BAD_REQUEST,"유효하지 않은 쿠폰 코드입니다."),
+    PAYMENT_INVALID_ORDER(HttpStatus.BAD_REQUEST,"유효하지 않은 주문입니다."),
+    PAYMENT_USER_NOT_FOUND(HttpStatus.NOT_FOUND,"사용자를 찾을 수 없습니다."),
+
+
+
+
+    ;
     private final HttpStatus httpStatus;
     private final String errorMsg;
 
