d20dff742c4ad24b8befdd2e4e6e0052b2ed9d96
Kyoungwon Min <mkkw2047@gmail.com>
2023-09-20 04:29:21 +0900

    chore: Spring Batch 환경 설정
    Spring Batch 환경 설정

--- src/main/java/com/github/commerce/CommerceApplication.java
+++ src/main/java/com/github/commerce/CommerceApplication.java
@@ -1,10 +1,12 @@
 package com.github.commerce;
 
+import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
+@EnableBatchProcessing //Spring Batch 사용 위해 필요
 @EnableScheduling //@Scheduled 위해 필요
 @EnableJpaAuditing //@CreatedAt 위해 필요
 @SpringBootApplication
