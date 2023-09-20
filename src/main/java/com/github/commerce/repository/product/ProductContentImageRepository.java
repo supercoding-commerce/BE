184475696731ee30ac775937f59eef604da488f2
doit-zero <131241458+doit-zero@users.noreply.github.com>
2023-09-20 15:57:02 +0900

    refactor : 상품 수정 기능 수정 중
    - 상품 수정 기능 수정 중입니다.

--- src/main/java/com/github/commerce/repository/product/ProductContentImageRepository.java
+++ src/main/java/com/github/commerce/repository/product/ProductContentImageRepository.java
@@ -3,8 +3,16 @@ package com.github.commerce.repository.product;
 import com.github.commerce.entity.ProductContentImage;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import java.util.List;
 
 public interface ProductContentImageRepository extends JpaRepository<ProductContentImage, Long> {
     List<ProductContentImage> findAllByProduct_Id(Long productId);
+
+    @Modifying
+    @Query("UPDATE ProductContentImage p SET p.imageUrl = ?1 WHERE p.imageUrl = ?2")
+    void updateByImageUrl(String newImageUrl, String deleteImageUrl);
+
+    void deleteByImageUrl(String imageUrl);
 }
