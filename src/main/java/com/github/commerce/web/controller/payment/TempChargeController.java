2b36a616046a4bd98c937b266bf0a002f0d7f82f
dabin <113006964+dabinHam@users.noreply.github.com>
2023-09-22 05:38:34 +0900

    refactor : 임시충전기능 주석처리
    - 충전기능 구현완료되어서 임시충전기능 주석처리하였습니다.

--- src/main/java/com/github/commerce/web/controller/payment/TempChargeController.java
+++ src/main/java/com/github/commerce/web/controller/payment/TempChargeController.java
@@ -11,18 +11,18 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@Api(tags = "임시 충전 API")
+//@Api(tags = "임시 충전 API")
 @RequiredArgsConstructor
 @RestController
-@RequestMapping("/v1/api/charges")
+//@RequestMapping("/v1/api/charges")
 public class TempChargeController {
 
     /*todo : role이 User인 경우만 충전, Seller 인경우 예외처리 | 추후 충전기능 완성 후 제거예정 */
 
     private final TempChargeService tempChargeService;
 
-    @ApiOperation(value = "기존 DB에서 직접 페이머니 입력하지 않은 계정만 가능| 100만 단위충전")
-    @PostMapping("/temp-charge")
+//    @ApiOperation(value = "기존 DB에서 직접 페이머니 입력하지 않은 계정만 가능| 100만 단위충전")
+//    @PostMapping("/temp-charge")
     public ResponseEntity<?> tempCharge(
             @AuthenticationPrincipal UserDetailsImpl userDetails
     ) {
